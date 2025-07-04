dde958e23d15b2d5fe6ec1ef8fce0bf7443f8003
nabeelamjadsheikh <131901574+nabeelamjadsheikh@users.noreply.github.com>
2025-07-04 18:45:56 +0500

    add button

--- app/page.tsx
+++ app/page.tsx
@@ -615,7 +615,14 @@ export default function Portfolio() {
                   href="https://app.keyhole.test/account-tracking/dashboard/1yKZMl/anas-ali"
                   className="bg-gradient-to-r from-cyan-600 to-cyan-700 p-4 rounded-full hover:from-cyan-700 hover:to-cyan-800 transition-all duration-300 transform hover:scale-110 shadow-lg shadow-cyan-500/25"
                 >
-                  Keyhole Dashboard
+                  Keyhole AT Dashboard
+                </a>
+                <a
+                  target="_blank"
+                  href="https://app.keyhole.test/hashtag-tracking/dashboard/3gxz7K/TagEventApplied"
+                  className="bg-gradient-to-r from-cyan-600 to-cyan-700 p-4 rounded-full hover:from-cyan-700 hover:to-cyan-800 transition-all duration-300 transform hover:scale-110 shadow-lg shadow-cyan-500/25"
+                >
+                  Keyhole HT Dashboard
                 </a>
               </div>
             </div>
